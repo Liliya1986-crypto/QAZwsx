b621e59e061f8a3e1b94363a53fc73ddd8daaf49
Liliya1986-crypto <89047633212l@gmail.com>
2025-09-30 20:21:33 +0300

    989

--- QAZwsx.py
+++ QAZwsx.py
@@ -1,1 +1,12 @@
 989
+importtelebot;b
+ot =telebot.TeleBot('8051689493:AAGQB6Ec2DaFYh8JZsosDwvOjTKSoiWcW00');#тут токен бота@bo
+t.message_handler(content_types=['text'])#слушаем ботаdef 
+get_text(message):   i
+f message.text =="Привет": #проверям сообщение от пользователя      
+  bot.send_message(message.from_user.id,"Здравствуй, мой дорогойдруг!") #отвечаем пользователю    elif
+ message.text =="/help":       bo
+t.send_message(message.from_user.id,"напиши: Привет")    else:  
+     bot.
+send_message(message.from_user.id,"я тебя не понимаю, напиши '/help'")bot.polling(n
+one_stop=True,interval=0)# ботпостоянно будет опрашивает сервер
