25e6995d74afdac36073e47c6a65aaef342bb4f4
Liliya1986-crypto <89047633212l@gmail.com>
2025-09-30 20:29:33 +0300

    989

--- QAZwsx.py
+++ QAZwsx.py
@@ -1,12 +1,14 @@
-989
-importtelebot;b
-ot =telebot.TeleBot('8051689493:AAGQB6Ec2DaFYh8JZsosDwvOjTKSoiWcW00');#тут токен бота@bo
-t.message_handler(content_types=['text'])#слушаем ботаdef 
-get_text(message):   i
-f message.text =="Привет": #проверям сообщение от пользователя      
-  bot.send_message(message.from_user.id,"Здравствуй, мой дорогойдруг!") #отвечаем пользователю    elif
- message.text =="/help":       bo
-t.send_message(message.from_user.id,"напиши: Привет")    else:  
-     bot.
-send_message(message.from_user.id,"я тебя не понимаю, напиши '/help'")bot.polling(n
-one_stop=True,interval=0)# ботпостоянно будет опрашивает сервер
+import telebot
+
+bot = telebot.TeleBot('8051689493:AAGQB6Ec2DaFYh8JZsosDwvOjTKSoiWcW00')  # тут токен бота
+
+@bot.message_handler(content_types=['text'])  # слушаем бота
+def get_text(message):
+    if message.text == "Привет":  # проверяем сообщение от пользователя
+        bot.send_message(message.from_user.id, "Здравствуй, мой дорогой друг!")  # отвечаем пользователю
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "напиши: Привет")
+    else:
+        bot.send_message(message.from_user.id, "я тебя не понимаю, напиши '/help'")
+
+bot.polling(none_stop=True, interval=0)
